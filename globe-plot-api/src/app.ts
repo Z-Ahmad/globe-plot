07254dc11e9ab4c4a109070072cb73e440a9c988
Z-Ahmad <zakiahmad42661@gmail.com>
2025-06-13 00:08:30 -0400

    forgot https on cloudflare endpoint

--- globe-plot-api/src/app.ts
+++ globe-plot-api/src/app.ts
@@ -52,7 +52,14 @@ const geocodeLimiter = rateLimit({
 
 // Middleware
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174", "http://127.0.0.1:5173", "http://127.0.0.1:5174", "globe-plot.pages.dev", "https://globeplot.com"],
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:5174",
+    "https://globe-plot.pages.dev",
+    "https://globeplot.com"
+  ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
